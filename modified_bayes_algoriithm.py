d28f0eb235b67ea83dd244c3e9ebe566e65406f8
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-03-26 14:39:12 +0300

    implement log_naive_bayes function

--- modified_bayes_algoriithm.py
+++ modified_bayes_algoriithm.py
@@ -26,3 +26,52 @@ def log_prob_email_given_class(treated_email, cls, word_frequency, class_frequen
             prob += np.log(prob_word_given_class(word, cls, word_frequency, class_frequency))
 
     return prob
+
+
+def log_naive_bayes(treated_email, word_frequency, class_frequency, return_likelihood=False) :
+    """
+    Naive Bayes classifier for spam detection, comparing the log probabilities instead of the actual probabilities.
+
+    This function calculates the log probability of an email being spam (1) or ham (0)
+    based on the Naive Bayes algorithm. It uses the conditional probabilities of the
+    treated_email given spam and ham, as well as the prior probabilities of spam and ham
+    classes. The final decision is made by comparing the calculated probabilities.
+
+    Parameters:
+    - treated_email (list): A preprocessed representation of the input email.
+    - return_likelihood (bool): If true, it returns the log_likelihood of both spam and ham.
+
+    Returns:
+    - int: 1 if the email is classified as spam, 0 if classified as ham.
+    """
+
+    # Compute P(email | spam) with the new log function
+    log_prob_email_given_spam = log_prob_email_given_class(treated_email, cls='spam', word_frequency=word_frequency,
+                                                           class_frequency=class_frequency)
+
+    # Compute P(email | ham) with the function you defined just above
+    log_prob_email_given_ham = log_prob_email_given_class(treated_email, cls='ham', word_frequency=word_frequency,
+                                                          class_frequency=class_frequency)
+
+    # Compute P(spam) using the class_frequency dictionary and using the formula #spam emails / #total emails
+    p_spam = class_frequency['spam'] / (class_frequency['ham'] + class_frequency['spam'])
+
+    # Compute P(ham) using the class_frequency dictionary and using the formula #ham emails / #total emails
+    p_ham = class_frequency['ham'] / (class_frequency['ham'] + class_frequency['spam'])
+
+    # Compute the quantity log(P(spam)) + log(P(email | spam)), let's call it log_spam_likelihood
+    log_spam_likelihood = np.log(p_spam) + log_prob_email_given_spam
+
+    # Compute the quantity P(ham) * P(email | ham), let's call it ham_likelihood
+    log_ham_likelihood = np.log(p_ham) + log_prob_email_given_ham
+
+    # In case of passing return_likelihood = True, then return the desired tuple
+    if return_likelihood == True :
+        return (log_spam_likelihood, log_ham_likelihood)
+
+    # Compares both values and choose the class corresponding to the higher value.
+    # As the logarithm is an increasing function, the class with the higher value retains this property.
+    if log_spam_likelihood >= log_ham_likelihood :
+        return 1
+    else :
+        return 0
